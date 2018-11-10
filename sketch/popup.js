007d4081681228f86b54cdcb0cabe48a0300b5e6
Yazan Alhalabi <yazanalhalabi1@gmail.com>
2018-11-10 17:39:43 +0200

    adding multi lang

--- sketch/popup.js
+++ sketch/popup.js
@@ -2,60 +2,85 @@ function setup() {
   noCanvas();
   let bgpage = chrome.extension.getBackgroundPage();
   let text = bgpage.word;
+  chrome.tabs.detectLanguage(function(lang) {
+    const ISOlanguageCODE = lang;
 
-  const POST_DATA_FOR_ANNIF = {
-    project_id: 'yso-en',
-    text,
-    limit: 3,
-  };
+    const project_id = `yso-${ISOlanguageCODE}`;
+    const url = `http://api.annif.org/v1/projects/${project_id}/analyze`;
+    const POST_DATA_FOR_ANNIF = {
+      project_id,
+      text,
+      limit: 3,
+    };
 
-  $.ajax({
-    url: 'http://api.annif.org/v1/projects/yso-en/analyze',
-    method: 'POST',
-    data: POST_DATA_FOR_ANNIF,
-    success: finished,
-  });
+    $.ajax({
+      url,
+      method: 'POST',
+      data: POST_DATA_FOR_ANNIF,
+      success: finished,
+    });
 
-  function finished(response) {
-    response.results.map(key => {
-      //Keyword from ANNIF API
-      const keyword = key.label;
+    function finished(response) {
+      const annifLabels = [];
+      response.results.map(key => {
+        //Keyword from ANNIF API
+        const keyword = key.label;
+        const filter = {
+          en: 'eng',
+          fi: 'fin',
+          sv: 'swe',
+        };
+        // DATA for GET REQUEST to FINNA API
+        const GET_DATA_FOR_FINNA = {
+          lookfor: keyword,
+          type: 'Subject',
+          field: ['id', 'title', 'nonPresenterAuthors', 'images'],
+          filter: [`language: ${filter[ISOlanguageCODE]}`],
+          sort: 'relevance,id asc',
+          limit: 3,
+          prettyPrint: false,
+          lng: 'en-gb',
+        };
 
-      // DATA for GET REQUEST to FINNA API
-      const GET_DATA_FOR_FINNA = {
-        lookfor: keyword,
-        type: 'Subject',
-        field: ['id', 'title', 'nonPresenterAuthors', 'images'],
-        filter: ['language: eng'],
-        sort: 'relevance,id asc',
-        limit: 3,
-        prettyPrint: false,
-        lng: 'en-gb',
-      };
-
-      $.ajax({
-        type: 'GET',
-        url: 'https://api.finna.fi/api/v1/search',
-        data: GET_DATA_FOR_FINNA,
-        success: function(data) {
-          if (data.resultCount > 0) {
+        $.ajax({
+          type: 'GET',
+          url: 'https://api.finna.fi/api/v1/search',
+          data: GET_DATA_FOR_FINNA,
+          success: function(data) {
             data.records.map(values => {
               interface(values);
             });
-          } else {
-            console.log('no results');
-          }
-        },
+          },
+        });
+
+        // To show the subject/keyword that is used for the search
+        const bookBased = document.querySelector('.parent p');
+        const subject = document.createElement('span');
+        subject.textContent = keyword;
+        bookBased.appendChild(subject);
+
+        annifLabels.push(encodeURI(keyword));
       });
 
-      // To show the subject/keyword that is used for the search
-      const keywords = key.label;
-      const bookBased = document.querySelector('.parent p');
-      const subject = document.createElement('span');
-      subject.textContent = keywords;
-      bookBased.appendChild(subject);
-    });
-  }
+      // Link for show more button that sends to Finna library with the same keywords are used from Annif
+      const toFinna = `
+    https://www.finna.fi/Search/Results?sort=relevance&bool0%5B%5D=OR&lookfor0%5B%5D=
+    ${annifLabels[0]}
+    &type0%5B%5D=Subject&lookfor0%5B%5D=
+    ${annifLabels[1]}
+    &type0%5B%5D=Subject&lookfor0%5B%5D=
+    ${annifLabels[2]}
+    &type0%5B%5D=Subject&join=AND&limit=20`;
+
+      const parent = document.querySelector('.parent');
+      const showMore = document.createElement('a');
+      showMore.setAttribute('class', 'show-more');
+      showMore.href = toFinna.trim();
+      showMore.textContent = 'Show More';
+      showMore.target = '_blank';
+      parent.appendChild(showMore);
+    }
+  });
 }
 
 // Printing to the UI
